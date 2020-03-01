bbb541d18a6cd888695f7af92735c1a15010483a
Rakhimov, George <george.rakhimov@kronos.com>
2020-03-01 18:03:25 +0300

    Added DB connection

--- src/main/java/controllers/Application.java
+++ src/main/java/controllers/Application.java
@@ -6,12 +6,34 @@ import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConf
 import org.springframework.context.annotation.*;
 import org.springframework.boot.SpringApplication;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 @SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
 public class Application {
+
     public static void main(String[] args) {
+        Connection connection;
+        Statement statement;
+        try {
+            Class.forName("org.postgresql.Driver");
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/rabbits",
+                    "postgres", "123");
+            System.out.println("Database Connected ..");
+            statement = connection.createStatement();
+            String createTable = "Create Table if not exists cages(id int primary key, name varchar, address text)";
+            statement.executeUpdate(createTable);
+            statement.close();
+            connection.close();
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+
         SpringApplication.run(Application.class, args);
     }
 }
