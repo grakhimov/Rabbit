983ece1387b297d5d612b4df53b38847ddba48be
Rakhimov, George <george.rakhimov@kronos.com>
2020-03-10 23:35:43 +0300

    Added DB connection

--- src/main/java/com/rabbits/controllers/CageController.java
+++ src/main/java/com/rabbits/controllers/CageController.java
@@ -4,19 +4,22 @@ import com.rabbits.domain.Cage;
 import com.rabbits.exceptions.CageBadRequestException;
 import com.rabbits.repository.CageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class CageController {
     @Autowired
     private CageRepository cageRepository;
 
-    @GetMapping("/cages")
+    @GetMapping(value = "/cages")
     public ResponseEntity<List<Cage>> getCages() {
         return ResponseEntity.ok(cageRepository.findAll());
     }
@@ -27,24 +30,19 @@ public class CageController {
         return ResponseEntity.ok(cage);
     }
 
-    /*@PutMapping("/cages/{cageId}")
-    public ResponseEntity<Cage> updateCage(@PathVariable long cageId, @Valid @RequestBody Cage cage) {
-        Cage c = cageRepository.findById(cageId).orElseThrow(() ->
-                new CageBadRequestException(cageId));
-        c.setEntity(cage.getEntity());
-        c.setPos_x(cage.getPos_x());
-        c.setPos_y(cage.getPos_y());
-        final Cage updatedCage = cageRepository.save(c);
-        return ResponseEntity.ok(updatedCage);
-    }*/
-
     @PutMapping("/cages/{cageId}")
-    public ResponseEntity<Cage> updateCagePosition(@PathVariable long cageId, @RequestBody HashMap<String, Integer> body) {
+    public ResponseEntity<Cage> updateCage(@PathVariable long cageId, @RequestBody HashMap<String, Integer> body) {
         Cage c = cageRepository.findById(cageId).orElseThrow(() ->
                 new CageBadRequestException(cageId));
-        c.setPos_x(body.get("pos_x"));
-        c.setPos_y(body.get("pos_y"));
-        c.setEntity(body.get("entity"));
+        if (body.get("pos_x") != null) {
+            c.setPos_x(body.get("pos_x"));
+        }
+        if (body.get("pos_y") != null) {
+            c.setPos_y(body.get("pos_y"));
+        }
+        if (body.get("entity") != null) {
+            c.setEntity(body.get("entity"));
+        }
         final Cage updatedCage = cageRepository.save(c);
         return ResponseEntity.ok(updatedCage);
     }
@@ -55,5 +53,11 @@ public class CageController {
                 new CageBadRequestException(cageId));
         cageRepository.delete(c);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/cages/{cageId}")
+    public ResponseEntity<Cage> getCage(@PathVariable long cageId) {
+        return ResponseEntity.ok(cageRepository.findById(cageId).orElseThrow(() ->
+                new CageBadRequestException(cageId)));
     }
 }
